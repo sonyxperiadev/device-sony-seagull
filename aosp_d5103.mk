c7af1dd2d3ac5c98f805564fd0c3fa806eb9420f
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2015-04-28 14:56:58 +0200

    inherit-product cleanup
    make existing inherit-product mandatory and fail build
    if anything is missing
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- aosp_d5103.mk
+++ aosp_d5103.mk
@@ -18,7 +18,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
 $(call inherit-product, device/sony/yukon/device.mk)
 $(call inherit-product, vendor/sony/seagull/seagull-vendor.mk)
-$(call inherit-product-if-exists, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
     device/sony/seagull/overlay
